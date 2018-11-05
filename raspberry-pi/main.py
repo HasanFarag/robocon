353cb932710d31b0b2b243527eaec5caa7a1fca3
Bill Crawford <bill.crawford@forthvalley.ac.uk>
2018-11-05 23:17:46 +0000

    added boilerplate code for main program.

---
+++ raspberry-pi/main.py
@@ -0,0 +1,31 @@
+"""Rover application.
+
+This is the main entry point for the rover application.
+It is set up to act in a similar way to the arduino by using
+setup and loop functions.
+"""
+# we may change approach later but this will do for now.
+
+
+def setup():
+    """Docstring."""
+    # enter setup code here and delete pass
+    pass
+
+
+def loop():
+    """Docstring."""
+    # enter code that will contnously run here and delete pass
+    pass
+
+
+# IMPORTANT - DO NOT MODIFY ANY CODE BELOW THIS POINT
+def main():
+    """Docstring."""
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == '__main__':
+    main()
