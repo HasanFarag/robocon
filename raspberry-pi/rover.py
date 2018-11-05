5a3ecb12a2484d91caf78e43b23f381b14543531
Bill Crawford <bill.crawford@forthvalley.ac.uk>
2018-11-05 23:45:15 +0000

    added template code for the rover.

---
+++ raspberry-pi/rover.py
@@ -0,0 +1,16 @@
+"""Library containing the rover's functions."""
+import hardware as hw
+
+
+class Rover:
+    """Properties and methods of the rover."""
+
+    # TODO Properties. What are the attributes and properties
+    # of the rover?
+
+    # Example - The rover has a processor board (raspberry pi)
+    board = hw.Board  # This is how we create an object in python.
+    boardType = board.type()
+
+    # TODO Methods. What are the actions the rover has to be able
+    # to perform?
